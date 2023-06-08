1b80ea6d0cf3c97d465383ef313a80ee10d17c8a
rootraven04 <120294924+rootraven04@users.noreply.github.com>
2023-06-08 19:57:28 +0700

    Update nan.sh

--- nan.sh
+++ nan.sh
@@ -1,19 +1,24 @@
 #!/bin/bash 
 # Reina Akiara v.0.1
 
+
+#		---	PANG
+red="\033[1;31m"
+green="\033[1;32m"
+blue="\033[1;34m"
+norm="\033[0m"
+
 source_campaign="http://198.148.116.171/666/xAGCx/campaign.zip"
 xml_agc="https://raw.githubusercontent.com/rootraven04/named/main/agc.xml"
 sql_agc="https://raw.githubusercontent.com/rootraven04/named/main/agc.txt"
-
+owner=$(echo -e "$(pwd)" | cut -d "/" -f3)
+if [[ $(id | grep "root" ) ]]; then extra="--allow-root"; else extra=""; fi
 
 
 _INSTALLWP() {
-	owner=$(echo -e "$(pwd)" | cut -d "/" -f3)
-	if [[ $(id | grep "root" ) ]]; then extra="--allow-root"; else extra=""; fi
-
 	echo -e ""
 	#	----	VAR		----
-	echo -e "[+][STEP 1]==================================="
+	echo -e "[+][STEP 1]-----------------------------------"
 	echo ""
 	read -p $" [+] Directory name: " dir
 	read -p $" [+] DB HOST       : " dbhost
@@ -22,19 +27,19 @@ _INSTALLWP() {
 	read -p $" [+] DB PASSWORD   : " dbpass
 	read -p $" [+] DB PREFIX     : " dbprefix
 	echo ""
-	echo -e "[+][STEP 2]==================================="
+	echo -e "[+][STEP 2]----------------------------------"
 	echo ""
-	read -p $" [+] URL [e.g:example.com]     : " url
-	read -p $" [+] TITLE                     : " title
-	read -p $" [+] ADMIN USERNAME            : " username
-	read -p $" [+] ADMIN PASSWORD            : " password
-	read -p $" [+] ADMIN EMAIL               : " email
+	read -p $" [+] URL [e.g:example.com/blog/]   : " url
+	read -p $" [+] TITLE                         : " title
+	read -p $" [+] ADMIN USERNAME                : " username
+	read -p $" [+] ADMIN PASSWORD                : " password
+	read -p $" [+] ADMIN EMAIL                   : " email
 
 	echo -e ""
-	echo -e "[~] Downloading Wordpress"
-	wget -q https://wordpress.org/latest.zip && unzip -q "latest.zip" && mv "wordpress" ${dir} && cd ${dir}
+	echo -e "${blue}[~]${norm} Downloading Wordpress"
+	wget -q https://wordpress.org/latest.zip && unzip -q "latest.zip" && rm -rf "latest.zip" && mv "wordpress" ${dir} && cd ${dir}
 	
-	echo -e "[~] Downloading wp-cli.phar..."
+	echo -e "${blue}[~]${norm} Downloading wp-cli.phar..."
 	curl -sO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 	#	Creating config file
@@ -42,13 +47,14 @@ _INSTALLWP() {
 	php wp-cli.phar core install --url="${url}" --title="${title}" --admin_user="${username}" --admin_email="${email}" $extra
 	php wp-cli.phar user list $extra
 
+	echo -e "${blue}[~]${norm} Updating your default password..."
 	php wp-cli.phar user update 1 --user_pass="${password}" $extra
 
 	php wp-cli.phar db query "UPDATE $(php wp-cli.phar db prefix $extra)options SET option_value = 'http://${url}' WHERE option_name = 'siteurl'" $extra
 	php wp-cli.phar db query "UPDATE $(php wp-cli.phar db prefix $extra)options SET option_value = 'http://${url}' WHERE option_name = 'home'" $extra
 	
-	echo -e "[+] http://${url}/wp-login.php"
-	echo -e "[+] Username: ${user} [+] password: ${password} | Have Fun ntol!"
+	echo -e "${green}[+]${norm} http://${url}/wp-login.php"
+	echo -e "${green}[+] Username:${norm} ${username}${green} [+] password:${norm} ${password} | ${green}Have Fun ntol! ${norm}"
 
 #
 #	echo -e "[+] ========================================= [+]"
@@ -91,10 +97,10 @@ _INSTALLAGC() {
 
 	curl -sO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-	echo -e "[~] Changing Permalinks..."
+	echo -e "${blue}[~]${norm} Changing Permalinks..."
 		php wp-cli.phar db query "UPDATE $(php wp-cli.phar db prefix $extra)options SET option_value = '/%postname%/' WHERE option_name = 'permalink_structure'"
 		php wp-cli.phar db query "SELECT * FROM $(php wp-cli.phar db prefix $extra)options WHERE option_name = 'permalink_structure'"
-	echo -e "[~] Installing Plugin..."
+	echo -e "${blue}[~]${norm} Installing Plugin..."
 	php wp-cli.phar plugin install ${source_campaign} --activate
 	#php wp-cli.phar cron event list $extra
 
@@ -104,42 +110,44 @@ _INSTALLAGC() {
 	wget ${sql_agc} -O agc.sql
 	sed -i -e 's/blogwpx_/'"$(php wp-cli.phar db prefix $extra)"'/g' agc.sql
 	php wp-cli.phar db import agc.sql
-	php wp-cli.phar user create ${username} admin@gmail.com --role=administrator
-	echo -e "[+] Username: ${username}"
+	php wp-cli.phar user create ${username} adminwordpress@mailwordpress.com --role=administrator
+	echo -e "${green}[+] Username:${norm} ${username}"
 }
 
 
 
-echo -e "
-
-
-██████╗ ███████╗██╗███╗   ██╗ █████╗      █████╗ ██╗  ██╗██╗ █████╗ ██████╗  █████╗ 
-██╔══██╗██╔════╝██║████╗  ██║██╔══██╗    ██╔══██╗██║ ██╔╝██║██╔══██╗██╔══██╗██╔══██╗
-██████╔╝█████╗  ██║██╔██╗ ██║███████║    ███████║█████╔╝ ██║███████║██████╔╝███████║
-██╔══██╗██╔══╝  ██║██║╚██╗██║██╔══██║    ██╔══██║██╔═██╗ ██║██╔══██║██╔══██╗██╔══██║
-██║  ██║███████╗██║██║ ╚████║██║  ██║    ██║  ██║██║  ██╗██║██║  ██║██║  ██║██║  ██║
-╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
-                                                                                    
-
-"
-echo -e "[~] Checking WP-CLI.phar is supported or not"
+echo -ne "${blue}[~]${norm} Checking WP-CLI.phar is supported or not...\r" && sleep 0.8
 curl -sO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 if [[ $(php wp-cli.phar $extra | grep "WP-CLI only works correctly from the command line") ]];
 then
-	echo -e "[!] wp-cli not supported!" && exit
+	echo -e "${red}[!]${norm} Sorry, Wp-cli is not supported!" && exit
 else
-	echo -e "[+] wp-cli supported!"
+	echo -e "${green}[+]${norm} Congrats, Wp-cli is supported!\r" && sleep 0.8
 fi
 
 rm -rf "wp-cli.phar"
 
 echo -e "
-[+] ===============================
 
-  [1] Install Wordpress & AGC
-  [2] Install AGC
 
-[+] ===============================
+   ██████╗ ███████╗██╗███╗   ██╗ █████╗      █████╗ ██╗  ██╗██╗ █████╗ ██████╗  █████╗ 
+   ██╔══██╗██╔════╝██║████╗  ██║██╔══██╗    ██╔══██╗██║ ██╔╝██║██╔══██╗██╔══██╗██╔══██╗
+   ██████╔╝█████╗  ██║██╔██╗ ██║███████║    ███████║█████╔╝ ██║███████║██████╔╝███████║
+   ██╔══██╗██╔══╝  ██║██║╚██╗██║██╔══██║    ██╔══██║██╔═██╗ ██║██╔══██║██╔══██╗██╔══██║
+   ██║  ██║███████╗██║██║ ╚████║██║  ██║    ██║  ██║██║  ██╗██║██║  ██║██║  ██║██║  ██║
+   ╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                                                                    
+
+"
+
+
+echo -e "
+  ===============================
+
+    [1] Install Wordpress
+    [2] Install AGC [Campaign]
+
+ ===============================
 "
 read -p $": Select Option : " option
 
@@ -153,10 +161,11 @@ elif [[ $option == "0" ]];
 	then
 		exit
 else
-	echo -e "sams"
+	echo -e "Pilihen Goblok!"
 fi
 
 
+rm -rf "agc.sql"
 rm -rf "agc.xml" && rm -rf "wp-cli.phar"
 #	php wp-cli.phar db query "UPDATE wp_options SET option_value = '%postname%' WHERE option_name = 'permalink_structure'"
 
